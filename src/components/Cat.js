b310a25036b36d90cf8ef47ec77191834d803a2b
Lucas Lederman <ljlederman@gmail.com>
2021-09-30 17:03:43 +0100

    more css improvement

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -4,21 +4,39 @@ const Cat = (props) => {
     return (
         <StyledCat>
             <img src={props.url} alt=""></img>
-            <p>{props.name}</p>
-            <p>Breed: {props.breed}</p>
-            <p>Desc: {props.desc}</p>
-            <p>£{props.price}</p>
-            <BasketButton name={props.name} price={props.price} breed={props.breed} url={props.url} desc={props.desc} HandleBasket={props.HandleBasket} HandleSend={props.HandleSend}/>
+            <div className="textBox">
+                <p>{props.name}</p>
+                <p>Breed: {props.breed}</p>
+                <p>Desc: {props.desc}</p>
+                <p>£{props.price}</p>
+                <BasketButton name={props.name} price={props.price} breed={props.breed} url={props.url} desc={props.desc} HandleBasket={props.HandleBasket} HandleSend={props.HandleSend}/>
+            </div>
         </StyledCat>
     );
 };
 
 const StyledCat = styled.div`
+display: flex;
+flex-direction: column;
 border: 2px solid black;
 width: 20%;
+min-height: 30%;
 text-align: center;
+margin-bottom: 30px;
     img{
         max-width: 100%;
+    }
+    .textBox {
+        display: flex;
+        border-top: 2px solid black;
+        justify-content: space-between;
+        align-items: center;
+        flex-direction: column;
+        background-color: #41B3A3;
+        flex: 1;
+    }
+    p{
+        width: 100%;
     }
 `
 export default Cat;
