8e93f7d1c19d5d3df1572ae48163d4921283330b
Benjamin Jones <benjamin.jones@clifton-asset.co.uk>
2021-10-01 11:14:50 +0100

    changed format to be used for react router

--- src/App.js
+++ src/App.js
@@ -1,70 +1,30 @@
 import './App.css';
-import Cat from "./components/Cat.js";
-import { useState, useEffect } from "react";
-import styled from "styled-components";
-import Cart from "./components/Cart"
-const faker = require("faker");
+import { useState } from "react"
+import Main from "./components/Main"
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 const App = () => {
-    const [theData, setData] = useState([]);
-    const [curprice, setPrice] = useState(0);
     const [cartObject, setCartObject] = useState({
-                                                  itemsInCart: [],
-                                                  showModal: false,
-                                                });
-
-    useEffect(() => {
-        HandleFetch();
-    }, []);
-
-    const HandleFetch = async () => {
-        let response = await fetch("https://api.thecatapi.com/v1/images/search?limit=12");
-        let data = await response.json();
-        for (let i = 0; i < data.length; i++) {
-            const item = data[i];
-            item.name = faker.name.findName();
-            item.price = faker.commerce.price();
-            item.breed = faker.animal.cat();
-            item.desc = faker.commerce.productAdjective();
-    }
-        setData(data);
-    };
-
-    const HandleBasket = (price) => {
-        setPrice(curprice + parseInt(price))
-    }
-
-    const HandleSend = (name, price, breed, url, desc) => {
-        let cat = {
-            name: name,
-            price: price,
-            breed: breed,
-            url: url,
-            desc: desc
-        };
-        let tempCart = cartObject;
-        tempCart.itemsInCart = [...tempCart.itemsInCart, cat];
-        setCartObject(tempCart);  
-    }
-
+        itemsInCart: [],
+        showModal: false,
+        wallet: "5000"
+    });
+    
     return (
-      <div>
-          <StyledTitle> Cats 4 Lyfe</StyledTitle>
-          <Cart cartObject={cartObject} setCartObject={setCartObject} />
-          <StyledPetBox className="petBox">
-          {theData.map((item, index) => (
-              <Cat key={index} url={item.url} name={item.name} price={item.price} breed={item.breed} desc={item.desc} HandleBasket={HandleBasket} HandleSend={HandleSend}/>))}
-        </StyledPetBox>
-      </div>
-  )
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <Main cartObject={cartObject} setCartObject={setCartObject} />
+                </Route>
+                <Route path="/:id"> {/*individual product page*/}
+
+                </Route>
+                <Route path="/checkOut">
+
+                </Route>
+            </Switch>
+        </Router>
+    )
 }
 
-const StyledPetBox = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
-`
-const StyledTitle = styled.h1`
-text-align: center;
-`
-export default App;
+export default App
---
+++ src/components/Main.js
@@ -0,0 +1,65 @@
+import Cat from "./Cat.js";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import Cart from "./Cart";
+const faker = require("faker");
+
+const Main = (props) => {
+    const [theData, setData] = useState([]);
+    const [curprice, setPrice] = useState(0);
+    
+
+    useEffect(() => {
+        HandleFetch();
+    }, []);
+
+    const HandleFetch = async () => {
+        let response = await fetch("https://api.thecatapi.com/v1/images/search?limit=12");
+        let data = await response.json();
+        for (let i = 0; i < data.length; i++) {
+            const item = data[i];
+            item.name = faker.name.findName();
+            item.price = faker.commerce.price();
+            item.breed = faker.animal.cat();
+            item.desc = faker.commerce.productAdjective();
+        }
+        setData(data);
+    };
+
+    const HandleBasket = (price) => {
+        setPrice(curprice + parseInt(price));
+    };
+
+    const HandleSend = (name, price, breed, url, desc) => {
+        let cat = {
+            name: name,
+            price: price,
+            breed: breed,
+            url: url,
+            desc: desc
+        };
+        let tempCart = props.cartObject;
+        tempCart.itemsInCart = [...tempCart.itemsInCart, cat];
+        props.setCartObject(tempCart);
+    };
+
+    return (
+        <div>
+            <StyledTitle> Cats 4 Lyfe</StyledTitle>
+            <Cart cartObject={props.cartObject} setCartObject={props.setCartObject} />
+            <StyledPetBox className="petBox">
+                {theData.map((item, index) => (
+                    <Cat key={index} url={item.url} name={item.name} price={item.price} breed={item.breed} desc={item.desc} HandleBasket={HandleBasket} HandleSend={HandleSend} />))}
+            </StyledPetBox>
+        </div>
+    );
+};
+const StyledPetBox = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+`;
+const StyledTitle = styled.h1`
+text-align: center;
+`;
+export default Main;
